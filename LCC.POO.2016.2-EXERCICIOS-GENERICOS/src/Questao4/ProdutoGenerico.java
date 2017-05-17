f0e849dbfafdf2541767ffa2b8e9130bbc0c762a
IVONILDOO <ivonildo.neto@hotmail.com>
2017-05-17 20:40:22 -0300

    atualizando

---
+++ LCC.POO.2016.2-EXERCICIOS-GENERICOS/src/Questao4/ProdutoGenerico.java
@@ -0,0 +1,30 @@
+package Questao4;
+
+public class ProdutoGenerico<T> {
+	private T codigo;
+	private String descricao;
+	private double preco;
+
+	public ProdutoGenerico(T cod, String descr, double pr) {
+		codigo = cod;
+		descricao = descr;
+		preco = pr;
+	}
+
+	public T getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	@Override
+	public String toString() {
+		return "ProdutoG{" + "Codigo = " + codigo + ",descricao = " + descricao + ",preco = " + preco + "}";
+	}
+}
