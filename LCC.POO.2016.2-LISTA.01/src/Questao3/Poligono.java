a4ce20ae621f7bb5805f81bd7ff5b62888317d42
IVONILDOO <ivonildo.neto@hotmail.com>
2017-06-03 19:27:38 -0300

    Lista de Thread

--- LCC.POO.2016.2-LISTA.01/src/Questao3/Poligono.java
+++ LCC.POO.2016.2-LISTA.01/src/Questao3/Poligono.java
@@ -1,5 +1,17 @@
 package Questao3;
 
+import java.util.ArrayList;
+
 public class Poligono {
+	
+	private ArrayList<Ponto> conjPont;
+	
+	public Poligono(){
+		conjPont = new ArrayList<Ponto>();
+		
+	}
+	
+	
+	
 
 }
--- LCC.POO.2016.2-LISTA.01/src/Questao9/TestCirculoDAsAreas.java
+++ LCC.POO.2016.2-LISTA.01/src/Questao9/TestCirculoDAsAreas.java
@@ -4,8 +4,8 @@ public class TestCirculoDAsAreas {
 	public static void main(String args){
 		CalculoDasAreas[] area = new CalculoDasAreas[]{ new quadrado(6), new retangulo(2,4)};
 		
-
-
+		
+System.out.println(area.toString());
 
 	}
 
