5ca191ab7bb6695a3fef581e33b409bb93329fd3
IVONILDOO <ivonildo.neto@hotmail.com>
2017-06-11 22:42:20 -0300

    Atualizando

--- LCC.POO.2016.2-LISTA.01/src/Questao2/Main.java
+++ LCC.POO.2016.2-LISTA.01/src/Questao2/Main.java
@@ -2,7 +2,8 @@ package Questao2;
 
 public class Main {
 	public static void main(String[] args){
-	Pessoa p1 = new Pessoa("ruck",18,1.66,65);
+		
+	Pessoa p1 = new Pessoa("ruck",60,1.65,66);
 	
 	
 	System.out.println(p1.toString());
